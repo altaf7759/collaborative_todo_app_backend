c2814991c78a59525ec1febd403d6484e6eba56d
Altaf Raja <altaf.1040216@gmail.com>
2026-01-15 10:37:01 +0530

    cors error resolved for both local and production

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ connectDB()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-      origin: "https://collaborative-todo-app-l4vy.vercel.app",
+      origin: ["https://collaborative-todo-app-l4vy.vercel.app", "http://localhost:5173"],
       credentials: true
 }))
 
